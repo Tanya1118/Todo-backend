fa6ecddb15d34884fe825f07cfd499bdfdc7bdb6
Tanya Khunteta <tanyakhunteta2003@gmail.com>
2024-10-20 23:07:32 +0530

    commit

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -11,11 +12,13 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // MongoDB connection (replace with your MongoDB connection string)
-mongoose.connect('mongodb+srv://tanyakhunteta2003:97IMCV1pOcKgRZbQ@cluster0.ogfil.mongodb.net/', {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
-.then(() => console.log('Connected to MongoDB'))
+.then(() => app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+}))
 .catch((error) => console.error('MongoDB connection error:', error));
 
 // Task Schema and Model
@@ -105,7 +108,5 @@ app.patch('/tasks/:id/toggle', async (req, res) => {
     }
 });
 
-// Start the server
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+
+
